7abe13272fcc5b24d887861fd90ea01c8384810c
Shashank Hegde <32035347+Shashank-Hegde@users.noreply.github.com>
2024-12-25 16:51:07 +0100

    Update intensity.py

--- intensity.py
+++ intensity.py
@@ -595,7 +595,7 @@ def extract_body_parts_clause(text):
 # -------------------------
 # SBERT-based Synonym Matching
 # -------------------------
-SYMPTOM_SYNONYM_THRESHOLD = 0.92
+SYMPTOM_SYNONYM_THRESHOLD = 1.00
 
 def map_synonym_with_sbert(user_input):
     """
